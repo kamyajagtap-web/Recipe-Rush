e531e167d2f74c2bac382289a1a76ebeaaf70e42
kamyajagtap-web <kamya.jagtap@gmail.com>
2026-04-19 16:10:08 -0700

    Add files via upload

--- cards/card1.gd
+++ cards/card1.gd
@@ -1,12 +1,14 @@
 extends Sprite2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(1.0).timeout
-	get_tree().change_scene_to_file("res://cards/card2.tscn")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	await get_tree().create_timer(Global.delay).timeout
+	Global.current_card += 1
+	var sequence = Global.easy
+	if Global.level == "medium":
+		sequence = Global.medium
+	elif Global.level == "hard":
+		sequence = Global.hard
+	if Global.current_card >= sequence.size():
+		get_tree().change_scene_to_file("res://bakearea.tscn")
+	else:
+		get_tree().change_scene_to_file(sequence[Global.current_card])
