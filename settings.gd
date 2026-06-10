092abe7fb991a27e35d71f98c334fdd915938469
kamyajagtap-web <kamya.jagtap@gmail.com>
2026-06-10 08:34:40 -0700

    Add files via upload

--- settings.gd
+++ settings.gd
@@ -1,4 +1,5 @@
+# Exits out of settings to the view the user was last on
 extends Node2D
 
-func _on_texture_button_pressed():
+func _on_exit_pressed():
 	get_tree().change_scene_to_file(Global.previous_scene)
